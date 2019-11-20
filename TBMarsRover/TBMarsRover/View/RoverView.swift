7c87d136e676ab8e871e2c21cfaf469461c4b38c
Tushar Bole <tushar.bole@suncorp.com.au>
2019-11-21 06:55:38 +1100

    Added extension to rotate image

---
+++ TBMarsRover/TBMarsRover/View/RoverView.swift
@@ -0,0 +1,21 @@
+//
+//  RoverView.swift
+//  TBMarsRover
+//
+//  Created by BOLE, Tushar on 21/11/19.
+//  Copyright © 2019 BOLE, Tushar. All rights reserved.
+//
+
+import UIKit
+
+class RoverView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
