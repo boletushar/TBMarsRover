b49b622ef9dc876766adc7d670c25e0427d27e6a
Tushar Bole <tushar.bole@suncorp.com.au>
2019-11-21 05:24:00 +1100

    Added Display protocol implementation to view controller

--- TBMarsRover/TBMarsRover/View/MarsRoverViewController.swift
+++ TBMarsRover/TBMarsRover/View/MarsRoverViewController.swift
@@ -8,18 +8,53 @@
 
 import UIKit
 
-class MarsRoverViewController: UIViewController {
+class MarsRoverViewController: UIViewController, MarsViewDisplay {
+
+    // MARK: - Outlets
 
     @IBOutlet weak var inputTextView: UITextView!
 
     @IBOutlet weak var outputLabel: UILabel!
 
+    // MARK: - Private variables
+
+    private(set) var presenter: MarsViewPresenting!
+
+    // MARK: - Lifecycle override
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
+        presenter = MarsViewPresenter(display: self)
     }
 
+    // MARK: - MarsViewDisplay
+
+    func showError(errorMessage: String) {
+
+    }
+
+    func setGrid(xPos: CGFloat, yPos: CGFloat) {
+
+    }
+
+    func setRovers(rovers: [Rover]) {
+
+    }
+
+    func navigate(rover: Rover, atIndex: Int, for path: RoverNavigationPath, withdelay: TimeInterval) {
+
+    }
+
+    func setOutputText(rovers: [Rover]) {
+
+    }
+
+    // MARK: - Button action
+
     @IBAction func didTapSubmitButton(_ sender: UIButton) {
+        presenter.processInput(inputTextView.text)
     }
 }
 
