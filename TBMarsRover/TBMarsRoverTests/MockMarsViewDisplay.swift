859113ce7fa7fc02327703d9d390522a542f858e
Tushar Bole <tushar.bole@suncorp.com.au>
2019-11-21 08:04:46 +1100

    Added Unit test target

---
+++ TBMarsRover/TBMarsRoverTests/MockMarsViewDisplay.swift
@@ -0,0 +1,9 @@
+//
+//  MockMarsViewDisplay.swift
+//  TBMarsRoverTests
+//
+//  Created by BOLE, Tushar on 21/11/19.
+//  Copyright © 2019 BOLE, Tushar. All rights reserved.
+//
+
+import Foundation
